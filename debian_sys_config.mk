8a6424f8d88ad2a4c7db5539c02c9b2b698de73c
shawnstone <328494516@qq.com>
2019-05-23 23:48:09 -0400

    Update debian_sys_config.mk

--- debian_sys_config.mk
+++ debian_sys_config.mk
@@ -17,4 +17,14 @@ apt-get install build-essential
 
 sh ./VBoxLinuxAddition.run
 
+ apt install net-tools
+
+
+# Using Ubuntu
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Using Debian, as root
+curl -sL https://deb.nodesource.com/setup_6.x | bash -
+apt-get install -y nodejs
 
