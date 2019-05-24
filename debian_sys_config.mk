6ba80d7a99647d27a2c757a98a9fe70837e28c57
shawnstone <328494516@qq.com>
2019-05-24 10:29:20 +0800

    Create debian_sys_config.mk
    增加部分debian系统配置命令

---
+++ debian_sys_config.mk
@@ -0,0 +1,20 @@
+
+
+
+
+//更新apt源 
+apt-get update 
+
+apt-get install sudo
+
+//安装gcc编译器 
+apt install gcc 
+
+//更新linux header 
+apt-get install linux-headers-$(uname -r) 
+
+apt-get install build-essential 
+
+sh ./VBoxLinuxAddition.run
+
+
