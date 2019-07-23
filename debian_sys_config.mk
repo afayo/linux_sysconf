34c36c53082298a508718d444771c92ff279f1f3
shawnstone <328494516@qq.com>
2019-07-23 11:16:29 +0800

    Update debian_sys_config.mk
    add some vim config

--- debian_sys_config.mk
+++ debian_sys_config.mk
@@ -28,3 +28,10 @@ sudo apt-get install -y nodejs
 curl -sL https://deb.nodesource.com/setup_6.x | bash -
 apt-get install -y nodejs
 
+#配置gvim支持中文，修改.bashrc文件
+echo “export LANG="zh_CN.UTF-8" ” > ~/.bashrc && source ~/.bashrc
+
+#安裝gvim需要用到的命令，ctags和cscope
+sudo apt install  exuberant-ctags  cscope
+
+
